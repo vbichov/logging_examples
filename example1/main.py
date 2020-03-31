6c1542e424a74e6d29db6d47edadb3ddc8d888d4
Vitaly Bichov <vitalyb@wix.com>
2020-03-31 17:34:10 +0300

    some comments

--- example1/main.py
+++ example1/main.py
@@ -1,5 +1,8 @@
 import logging
 
+
+#logging.basicConfig(level=logging.DEBUG)
+
 logging.debug('This is a debug message')
 logging.info('This is an info message')
 logging.warning('This is a warning message')
